da46a67e06dbfe55ca8fa1045a6bfc718e307a10
Hyeyun Shin <h3shin@uwaterloo.ca>
2013-01-28 18:41:59 -0500

    Jamie - Added additional files for music settings

---
+++ TheRoom/src/com/room/Options.java
@@ -0,0 +1,31 @@
+package com.room;
+import com.room.R;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class Options extends PreferenceActivity {
+
+	   private static final String OPT_MUSIC = "bgmusic";
+	   private static final boolean OPT_MUSIC_DEF = true;
+	   private static final String OPT_SOUNDS = "soundeffect";
+	   private static final boolean OPT_SOUNDS_DEF = true;   
+
+	   @Override
+	   protected void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      addPreferencesFromResource(R.xml.options);
+	   }
+
+	   public static boolean getBGMusic(Context context) {
+	      return PreferenceManager.getDefaultSharedPreferences(context)
+	            .getBoolean(OPT_MUSIC, OPT_MUSIC_DEF);
+	   }
+	   
+	   public static boolean getSoundEffect(Context context) {
+	      return PreferenceManager.getDefaultSharedPreferences(context)
+	            .getBoolean(OPT_SOUNDS, OPT_SOUNDS_DEF);
+	   }
+}
