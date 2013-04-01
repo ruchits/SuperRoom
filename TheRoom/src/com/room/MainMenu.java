e3e7890e7aca3e97bb694e0961575492c4dab2b9
Will <h3shin@uwaterloo.ca>
2013-03-31 23:59:19 -0400

    Will - day2 video

--- TheRoom/src/com/room/MainMenu.java
+++ TheRoom/src/com/room/MainMenu.java
@@ -42,7 +42,7 @@ public class MainMenu extends SSceneActivity
     	}
     	else if (box.name.equals("credits"))
     	{
-    		showCredits();
+    		//showCredits();
     	}      	
     	else if (box.name.equals("quit"))
     	{
--- TheRoom/src/com/room/media/MVideoActivity.java
+++ TheRoom/src/com/room/media/MVideoActivity.java
@@ -22,7 +22,7 @@ public class MVideoActivity extends Activity implements OnTouchListener{
 	private int stopPosition = 0;
 	
 	public static final String DAY1_VIDEO = "android.resource://com.room/raw/day1";
-	public static final String DAY2_VIDEO = "android.resource://com.room/raw/sample";
+	public static final String DAY2_VIDEO = "android.resource://com.room/raw/day2";
 	public static final String DAY3_VIDEO = "android.resource://com.room/raw/sample";
 	public static final String DAY4_VIDEO = "android.resource://com.room/raw/sample";
 	public static final String DAY5_VIDEO = "android.resource://com.room/raw/sample";
